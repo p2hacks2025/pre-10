3c21d14f26ad1175c39f42fc622697614eca1746
Shuto0577 <shuto.17_0707@icloud.com>
2025-12-15 20:11:35 +0900

    ・夜空シーンの仮作成（中身無し） ・CnstellationDataのコメントデータ格納場所の追記

--- Assets/con_stella/WorkSpace/Shuto/Scripts/ConstellationData.cs
+++ Assets/con_stella/WorkSpace/Shuto/Scripts/ConstellationData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Junya;
 
 [Serializable]
 public class ConstellationData
@@ -8,6 +9,8 @@ public class ConstellationData
     public string createdAt;
     public List<StarData> stars = new List<StarData>();
     public List<ConnectionData> connections = new List<ConnectionData>();
+    public Node<String> root = new Node<String>();
+
 }
 
 [Serializable]
